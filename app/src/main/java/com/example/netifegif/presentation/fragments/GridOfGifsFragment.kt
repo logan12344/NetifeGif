efb8e928d5ceccc724ed3adb1003b904c35511b1
Pavlo Horbatiuk <list12344@gmail.com>
2022-08-04 14:26:43 +0300

    List, grid button

--- app/src/main/java/com/example/netifegif/presentation/fragments/GridOfGifsFragment.kt
+++ app/src/main/java/com/example/netifegif/presentation/fragments/GridOfGifsFragment.kt
@@ -7,9 +7,13 @@ import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netifegif.R
 import com.example.netifegif.databinding.FragmentGridOfGifsBinding
+import com.example.netifegif.preference.Prefs
 import com.example.netifegif.presentation.GifsViewModel
 import com.example.netifegif.presentation.adapters.GifsAdapter
 import kotlinx.coroutines.Job
@@ -24,12 +28,14 @@ class GridOfGifsFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: GifsAdapter
     private val viewModel: GifsViewModel by sharedViewModel()
+    private lateinit var shared : Prefs
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         // Inflate the layout for this fragment
+        shared = Prefs(requireContext())
         binding = FragmentGridOfGifsBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -65,11 +71,35 @@ class GridOfGifsFragment : Fragment() {
         viewModel.trendingGifs.observe(viewLifecycleOwner) { list ->
             adapter.differ.submitList(list)
         }
+
+        binding.changeView.setOnClickListener {
+            setLayoutManager()
+        }
+    }
+
+    private fun setLayoutManager() {
+        if (shared.viewTypeList) {
+            shared.viewTypeList = false
+            binding.changeView.setImageResource(R.drawable.ic_view_stream_24)
+            recyclerView.layoutManager = GridLayoutManager(context, 2)
+        } else {
+            shared.viewTypeList = true
+            binding.changeView.setImageResource(R.drawable.ic_dashboard_24)
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        }
+        adapter.notifyDataSetChanged()
     }
 
     private fun setupRecyclerView() {
         recyclerView = binding.recycle
         adapter = GifsAdapter()
+        if (shared.viewTypeList) {
+            binding.changeView.setImageResource(R.drawable.ic_dashboard_24)
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        } else {
+            binding.changeView.setImageResource(R.drawable.ic_view_stream_24)
+            recyclerView.layoutManager = GridLayoutManager(context, 2)
+        }
         recyclerView.adapter = adapter
     }
 }
